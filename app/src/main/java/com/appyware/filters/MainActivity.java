d823544939efc1e2c2cec74c176a650827a5a5f4
appyware <vatsal.bajpai05@gmail.com>
2015-11-17 08:48:59 +0530

    Updated Gradle to v23

--- app/src/main/java/com/appyware/filters/MainActivity.java
+++ app/src/main/java/com/appyware/filters/MainActivity.java
@@ -13,7 +13,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -26,7 +26,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     Drawable[] layers = new Drawable[2];
 
